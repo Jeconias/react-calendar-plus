b0821e62883cf220cbb61de5f67916d092b5e9dd
Jeconias Santos <jeconiass2009@hotmail.com>
2019-03-31 23:31:33 -0300

    Primeira Versão funcional

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # CalendarJS
 
 Esse é um calendário com JavaScript que tem como propósito ser fléxivel para a inserção de eventos ou lembretes em determinadas datas.
+
+
+`Detalhamento no próximo update.`
+
+##### Estilo atual do calendário:
+![Estilo atual do calendário](calendarJS.png)
--- js/calendar.js
+++ js/calendar.js
@@ -9,50 +9,51 @@ let Calendar = (function(){
 
         this.selected = null;
         this.bodyToRender = null;
+        this.eventsSave = null;
 
         // VERIFICAR SE O ANO EH BISSEXTO
-        const leap = function()
+        const leap = () =>
         {
             return ((container.global.currentYear % 100) !== 0) && ((container.global.currentYear % 4) === 0);
         }
 
         // RETORNAR O PRIMEIRO DIA DO MES
-        const firstDayMonth = function()
+        const firstDayMonth = () =>
         {
             const primeiroDiaMes = new Date(container.global.currentYear, container.global.currentMonth, 1);
             return primeiroDiaMes.getDay();
         }
 
         // QUANDO O MES FOR IGUAL A 0, VOLTAR PARA 11 E RETROCEDER UM ANO
-        const lastMonth = function()
+        const lastMonth = () =>
         {
             if(container.global.currentMonth != 0)
             {
                 container.global.currentMonth--;
-                return;
+            }else{
+                container.global.currentMonth = 11;
+                container.global.currentYear--;
             }
-
-            container.global.currentMonth = 11;
-            container.global.currentYear--;
+            newDate();
             return; 
         }
 
         // QUADO O MES FOR IGUAL A 11, VOLTAR PARA 0 E AVANCAR UM ANO
-        const nextMonth = function()
+        const nextMonth = () =>
         {
             if(container.global.currentMonth != 11)
             {
                 container.global.currentMonth++;
-                return;
+            }else{
+                container.global.currentMonth = 0;
+                container.global.currentYear++;
             }
-
-            container.global.currentMonth = 0;
-            container.global.currentYear++;
+            newDate();
             return; 
         }
 
         // RETORNA A QUANTIDADE DE DIAS DO MES ATUAL
-        const totalDays = function(currentMonth = null)
+        const totalDays = (currentMonth = null) =>
         {
 
             currentMonth = (currentMonth === null) ? container.global.currentMonth : currentMonth;
@@ -79,14 +80,27 @@ let Calendar = (function(){
         }
 
         // SETA UMA NOVA DATA
-        const newDate = function()
+        const newDate = () =>
         {
             container.global.currentDate.setFullYear(container.global.currentYear, container.global.currentMonth, container.global.currentDate);
+
             renderBody();
+
+            selector(container.tags.ids.calendarHeaderMonth);
+            newValueInTag(container.lang.months[container.global.currentMonth]);
+
+            selector(container.tags.ids.calendarHeaderYear);
+            newValueInTag(container.global.currentYear);
+
+            // CARREGAR OS EVENTOS NAS DATAS
+            addCalendarEvent();
+
+            // RECARREGAR O EVENTO PARA FECHAR OS DETALHES
+            eventNew(container.tags.ids.hiddenEventsDetails, 'click', hiddenEventsDetails);
         }
 
         // SELECIONAR TAGS
-        const selector = function(selector)
+        const selector = (selector) =>
         {
             if(selector == '' || selector == null) {
                 writeConsole('Error -> Enter the selector in the SELECTOR method.');
@@ -94,73 +108,238 @@ let Calendar = (function(){
             }
             this.selected = document.querySelectorAll(selector);
             return methodsPublic;
-        }.bind(this);
+        }
 
 
         // SETAR NOVO VALOR PARA A TAG/TAGS
-        const text = function(newText)
+        const text = (newText) =>
         {
-            return render(newText);
+            return newValueInTag(newText);
         }
 
         // SETAR UM NOVO ANO NAS TAGS SELECIONADAS
-        const currentYear = function(target = null)
+        const currentYear = (target = null) =>
         {
             if(target == null || target == '') { 
                 writeConsole('currentYear needs a valid parameter'); return false;
             }
             selector(target);
-            return render(container.global.currentYear);
+            return newValueInTag(container.global.currentYear);
         }
 
-        const currentMonth = function(target)
+        const currentMonth = (target) =>
         {
             if(target == null || target == '') {
                 writeConsole('currentMonth needs a valid parameter'); return false;
             }
             selector(target);
-            return render(container.lang.months[container.global.currentMonth]);
+            return newValueInTag(container.lang.months[container.global.currentMonth]);
         }
 
-        // RENDERIZAR
-        const render = function(newText)
+        // ALTERAR VALORES DAS TAGS
+        const newValueInTag = (newText, sel = null) =>
         {
-            Array.prototype.forEach.call(this.selected, function(element){
+            let selected = (sel == null) ? this.selected : sel;
+            Array.prototype.forEach.call(selected, function(element){
                 element.innerHTML = newText;
             });
             return methodsPublic;
-        }.bind(this);
-
-        const renderHeader = function()
-        {
-            let header = '';
         }
 
-        const renderBody = function(target)
+        const renderContainer = (target) =>
         {
-            const totalDaysInt = totalDays();
+            if(target == null || target == '' || target.substring(1, 0) != '#') { 
+                writeConsole('render needs a valid parameter. Ex: #calendarId'); return false;
+            }
+            // SELECIONAR O CONTAINER DO CALENDARIO
+            selector(target);
+
+            // SALVAR O CONTAINER DO CALENDARIO
+            this.bodyToRender = this.selected;
+
+            // RENDERIZAR O CABEÇALHO
+            newValueInTag(renderHeader());
+
+            // RENDERIZAR O CORPO
+            renderBody();
+
+            // ADICIONAR OS EVENTOS
+            eventNew(container.tags.classes.calendarActionBefore, 'click', lastMonth);
+            eventNew(container.tags.classes.calendarActionAfter, 'click', nextMonth);
+            eventNew(container.tags.ids.hiddenEventsDetails, 'click', hiddenEventsDetails);
+
+            return methodsPublic;
+        }
+
+        const renderHeader = () =>
+        {
+            return '\
+            <div class="calendarHeader">\
+                <div>\
+                    <div class="calendarActionBefore"><i></i></div>\
+                    <h1 id="calendarHeaderMonth">'+container.lang.months[container.global.currentMonth]+'</h1>\
+                    <span id="calendarHeaderYear">'+container.global.currentYear+'</span>\
+                    <div class="calendarActionAfter"><i></i></div>\
+                </div>\
+                <div class="calendarDaysWeek">\
+                    <ul>\
+                        <li>'+container.lang.daysWeek[0]+'</li>\
+                        <li>'+container.lang.daysWeek[1]+'</li>\
+                        <li>'+container.lang.daysWeek[2]+'</li>\
+                        <li>'+container.lang.daysWeek[3]+'</li>\
+                        <li>'+container.lang.daysWeek[4]+'</li>\
+                        <li>'+container.lang.daysWeek[5]+'</li>\
+                        <li>'+container.lang.daysWeek[6]+'</li>\
+                    </ul>\
+                </div>\
+            </div>';
+        }
+
+        const renderEventsDetails = () => 
+        {
+            let eventsContainer = document.createElement('div');
+            let buttonContainer = document.createElement('div');
+            let buttonEventsDetails = document.createElement('span');
+
+            buttonEventsDetails.id = container.tags.ids.hiddenEventsDetails.substring(1);
+            buttonContainer.appendChild(buttonEventsDetails);
+
+            eventsContainer.className = container.tags.classes.eventDetails.substring(1);
+            eventsContainer.id = container.tags.ids.eventDetails.substring(1);
+            eventsContainer.appendChild(buttonContainer);
+            eventsContainer.appendChild(document.createElement('ul'));
+            
+            return eventsContainer;
+        }
+
+        const renderBody = (callback) =>
+        {
+            const totalDaysInt          = totalDays();
             const totalDaysIntLastMonth = totalDays(container.global.currentMonth - 1);
-            let renderStr = '';
+            const totalDaysNextMonth    = 42 - (firstDayMonth() + totalDaysInt);
+            const today                 = new Date();
+            let renderStr               = '';
+            let appendChild             = true;
 
             for(let i = firstDayMonth(); i > 0; i--)
             {
-                renderStr += '<span>' + (totalDaysIntLastMonth - (i-1)) + '</span>';
+                renderStr += '<span data-date="'+ (totalDaysIntLastMonth - (i-1)) +'-'+ (container.global.currentMonth - 1) +'-'+ container.global.currentYear +'" class="calendarLastAndNextMonth">' + (totalDaysIntLastMonth - (i-1)) + '</span>';
             }
 
 
             for(let i = 1; i <= totalDaysInt; i++)
             {
-                renderStr += '<span>' + i + '</span>';
+                renderStr += (i == container.global.currentDay && 
+                    container.global.currentMonth === today.getMonth() && 
+                    container.global.currentYear === today.getFullYear()
+                ) ? '<span data-date="'+ i +'-'+ container.global.currentMonth +'-'+ container.global.currentYear +'" class="calendarToday"><span title="Hoje">' + i + '</span></span>' : '<span data-date="'+ i +'-'+ container.global.currentMonth +'-'+ container.global.currentYear +'">' + i + '</span>'
             }
 
+            
+            for(let i = 1; i <= totalDaysNextMonth; i++)
+            {
+                renderStr += '<span data-date="'+ i +'-'+ (container.global.currentMonth + 1) +'-'+ container.global.currentYear +'" class="calendarLastAndNextMonth">' + i + '</span>';
+            }
 
-            this.bodyToRender = (this.bodyToRender != null) ? this.bodyToRender : target;
+            let body = document.createElement('div');
+            body.className = container.tags.classes.calendarBody.substring(1);
+            body.innerHTML = renderStr;
+            body.appendChild(renderEventsDetails());
+            //body.style.transform = 'translateY(0%)';
 
-            selector(this.bodyToRender);
-            render(renderStr);
+            // VERIFICAR SE O BODY JÁ EXISTE
+            Array.prototype.forEach.call(this.bodyToRender[0].childNodes, function(element){
+                if(element.className === container.tags.classes.calendarBody.substring(1)){
+                    element.innerHTML = renderStr;
+                    element.appendChild(renderEventsDetails());
+                    appendChild = false;
+                }
+            });
 
+            if(appendChild){ this.bodyToRender[0].appendChild(body) }
+
+            return true;
+        }
+
+        const eventNew = (select, type, funct) =>
+        {
+            selector(select);
+
+            Array.prototype.forEach.call(this.selected, function(element){
+                element.addEventListener(type, funct, false);
+            });
+
+        }
+
+        const eventsShow = (event) =>
+        {
+            selector(container.tags.ids.eventDetails);
+            
+            const clickedSpan = event.target;
+            const lastParent = clickedSpan.parentElement;
+            const eventDetailsUl = this.selected[0].childNodes[1];
+            let list = '';
+            
+            this.eventsSave.events.forEach(function(element){
+                if(lastParent.getAttribute('data-date') === element.data)
+                {
+                    list += '<li><a href="#">'+ element.name +' - '+ element.hora +'</a></li>';
+                }
+            });
+
+            eventDetailsUl.innerHTML = list;
+            this.selected[0].style.transform = 'translateX(0)';
+        }
+
+        const hiddenEventsDetails = () => 
+        {
+            selector(container.tags.ids.eventDetails);
+            this.selected[0].style.transform = 'translateX(-100%)';
+        }
+
+        const addCalendarEvent = (eventsObj = null) =>
+        {
+            if(eventsObj != null)
+            {
+                this.eventsSave = eventsObj;
+            }
+
+            if(this.eventsSave == undefined || this.eventsSave === null) return {};
+
+            if(typeof this.eventsSave === 'object' && Array.isArray(this.eventsSave) === true)
+            {
+                writeConsole('You must pass a valid object');
+                return {};
+            }
+
+            let eventsLength = this.eventsSave.events.length;
+
+            selector(container.tags.classes.calendarBody);
+
+            Array.prototype.forEach.call(this.selected, function(element){
+
+                while(eventsLength && typeof this.eventsSave.events[eventsLength-1] === 'object' && Array.isArray(this.eventsSave.events[eventsLength-1]) === false)
+                {
+                    element.childNodes.forEach(function(span){
+                        if(span.getAttribute('data-date') == this.eventsSave.events[eventsLength-1].data)
+                        {
+                            let spanNew = document.createElement('span');
+                            spanNew.title = 'Visualizar eventos';
+                            spanNew.innerHTML = span.innerText;
+
+                            span.className = container.tags.classes.calendarEvent.substring(1);
+                            span.innerHTML = '';
+                            span.appendChild(spanNew);
+
+                            spanNew.addEventListener('click', eventsShow, false);
+                        }
+                    }.bind(this));
+                    eventsLength--;
+                }
+            }.bind(this));
+            
             return methodsPublic;
-        }.bind(this);
+        };
 
         // METODOS PUBLICOS PARA O USUARIO
         let methodsPublic = {
@@ -168,9 +347,8 @@ let Calendar = (function(){
             text        : text,
             currentYear : currentYear,
             currentMonth: currentMonth,
-            renderBody  : renderBody,
-            lastMonth   : lastMonth,
-            nextMonth   : nextMonth
+            render      : renderContainer,
+            addEvents   : addCalendarEvent
         }
 
         writeConsole('Finished.');
@@ -184,7 +362,7 @@ let Calendar = (function(){
         if(typeof obj === 'object' && Array.isArray(obj) === true || obj == '' || obj === null)
         {
             writeConsole('You must pass a valid object');
-            return false;
+            return undefined;
         }
 
         let paths = path.split('.'), size = paths.length, i = 0;
@@ -201,13 +379,14 @@ let Calendar = (function(){
 
     // METODO INIT QUE FAZ PAPEL DE CONSTRUTOR
     Calendar.init = function(config){
-        
+
         // CRIANDO OS OBJETOS NECESSARIOS
         this.container = {};
         let paths = {
             firstPath   :['lang', ['daysWeek', 'months'], 'array'],
             secondPath  :['global', 'object'],
             thirdPath   :['dev', 'object'],
+            fourPath    :['tags', ['ids', 'classes'], 'object']
         }
 
         // SIMPLES METODO PARA A CRIACAO DE PATHS DENTRO DO CONTAINER
@@ -247,11 +426,27 @@ let Calendar = (function(){
             currentDate     : localDate,
             currentYear     : localDate.getFullYear(),
             currentMonth    : localDate.getMonth(),
+            currentDay      : localDate.getDate()
         }
 
         this.container.dev = {
             name: 'Jeconias Santos',
             github: 'jeconiassantos/calendarjs'
+        }
+
+        this.container.tags.ids = {
+            eventDetails        : '#eventDetails',
+            calendarHeaderYear  : '#calendarHeaderYear',
+            calendarHeaderMonth : '#calendarHeaderMonth',
+            hiddenEventsDetails : '#hiddenEventsDetails'
+        }
+
+        this.container.tags.classes = {
+            eventDetails            : '.eventDetails',
+            calendarEvent           : '.calendarEvent',
+            calendarBody            : '.calendarBody',
+            calendarActionBefore    : '.calendarActionBefore',
+            calendarActionAfter     : '.calendarActionAfter',
         }
 
         // IDIOMA PADRAO DA SEMANA
@@ -281,10 +476,25 @@ let Calendar = (function(){
             'Dezembro'
         ];
 
-        //if(typeof config.lang.months) {console.log('a')};
-
-
-
+        if(config != null)
+        {
+            // VALIDAR OS DIAS DA SEMANA
+            if(verifyObject(config, 'lang.daysWeek') != undefined){ 
+                if(Array.isArray(config.lang.daysWeek) === false || config.lang.daysWeek.length < 7) {
+                    writeConsole('"daysWeek" must be an array with 7 values.');
+                }
+                this.container.lang.daysWeek = config.lang.daysWeek;
+            }
+    
+            // VALIDAR OS MESES
+            if(verifyObject(config, 'lang.months') != undefined){ 
+                if(Array.isArray(config.lang.months) === false || config.lang.months.length < 11) {
+                    writeConsole('"months" must be an array with 11 values.');
+                }
+                this.container.lang.months = config.lang.months;
+            }
+        }
+        
         writeConsole('Dev -> ' + this.container.dev.name);
         writeConsole('gitHub -> ' + this.container.dev.github);
         return new Calendar(this.container);
@@ -299,8 +509,8 @@ let Calendar = (function(){
             return;
         }
 
-        if(!this.instance) return this.instance = new Calendar.init(config);
-        return this.instance;
+        if(!instance) return instance = new Calendar.init(config);
+        return instance;
     }
 
 })();
