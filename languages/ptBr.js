974da9359b30b73b471f6a43ff0c5d0796e5b5b4
Jeconias Santos <jeconiass2009@hotmail.com>
2019-10-03 01:35:28 -0300

    Simple way to translate the calendar.

---
+++ languages/ptBr.js
@@ -0,0 +1,19 @@
+const ptBr = {
+  daysWeek: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+  months: [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+  ]
+};
+
+export default ptBr;
