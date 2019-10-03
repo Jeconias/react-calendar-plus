5dd9509ac34a9bea2706c9349d14924a9f153b60
Muktazam Habsi Ashidiqi <hasbeeazam@gmail.com>
2019-10-03 12:23:45 +0700

    Add Indonesia language

---
+++ languages/id_ID.js
@@ -0,0 +1,19 @@
+const id_ID = {
+  daysWeek: ["Min", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"],
+  months: [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+  ]
+};
+
+export default id_ID;
