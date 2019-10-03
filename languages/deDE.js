dac91105630d3e8d4163bfa58df31e4a97d1aa20
DrDrJojo <drdrjojo@users.noreply.github.com>
2019-10-03 12:42:47 +0200

    German language file added

---
+++ languages/deDE.js
@@ -0,0 +1,19 @@
+const ptBr = {
+  daysWeek: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+  months: [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+  ]
+};
+
+export default ptBr;
