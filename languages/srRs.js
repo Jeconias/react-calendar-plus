6bd79545639cee36640ad95e504ff4f16b79b8b5
Ivan Djurdjevac <djurdjevac@gmail.com>
2019-10-03 07:25:57 +0200

    Translation to Serbia - Serbian (Latin)

---
+++ languages/srRs.js
@@ -0,0 +1,19 @@
+const rsSr = {
+    daysWeek: ["Ned", "Pon", "Uto", "Sre", "Čet", "Pet", "Sub"],
+    months: [
+        "Januar",
+        "Februar",
+        "Mart",
+        "April",
+        "Maj",
+        "Jun",
+        "Jul",
+        "Avgust",
+        "Septembar",
+        "Oktobar",
+        "Novembar",
+        "Decembar"
+    ]
+};
+
+export default rsSr;
