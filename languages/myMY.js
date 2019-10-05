f360e936eac4ec99a39f468499be45eebaf961c2
Paskalouis Patressi <recoganjen@gmail.com>
2019-10-06 01:25:17 +0700

    Add Malay Language

---
+++ languages/myMY.js
@@ -0,0 +1,19 @@
+const myMY = {
+    daysWeek: ["Ahad", "Isn", "Sel", "Rab", "Kha", "Jum", "Sab"],
+    months: [
+        "Januari",
+        "Februari",
+        "Mac",
+        "April",
+        "Mei",
+        "Jun",
+        "Julai",
+        "Ogos",
+        "September",
+        "Oktober",
+        "November",
+        "Disember"
+    ]
+};
+
+export default myMY;
