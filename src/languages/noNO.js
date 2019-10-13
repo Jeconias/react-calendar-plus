c844c08cba7da261f588fac59dd074645179a53c
tubacase <tubacas3@gmail.com>
2019-10-13 20:36:08 +0200

    Add Norwegian (bokmål) translations

---
+++ src/languages/noNO.js
@@ -0,0 +1,21 @@
+module.exports = {
+    daysWeek: ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"],
+    months: [
+        "Januar",
+        "Februar",
+        "Mars",
+        "April",
+        "Mai",
+        "Juni",
+        "Juli",
+        "August",
+        "September",
+        "Oktober",
+        "November",
+        "Desember"
+    ],
+    settings: {
+        settings: "Innstillinger",
+        theme: "Fargetema"
+    },
+};
