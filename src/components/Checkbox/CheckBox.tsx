6fec28ed02d7e514fbb642434e252ba719a9e1b8
Jeconias Santos <jeconiass2009@hotmail.com>
2020-03-19 02:43:52 -0300

    :sparkles: Added Select and Checkbox component
    Issue: #42

---
+++ src/components/Checkbox/CheckBox.tsx
@@ -0,0 +1,62 @@
+import React, { useState, Fragment } from 'react';
+import styled, { css } from 'styled-components';
+import { rem, darken } from 'polished';
+import { CheckBoxInterface } from '@src//core/interfaces/CheckBoxInterface';
+
+const CheckBox: React.FC<CheckBoxInterface> = ({ label, onChange }) => {
+  const [checked, setChecked] = useState<boolean>(false);
+  const handleChange = () => {
+    setChecked(!checked);
+    if (onChange) onChange();
+  };
+
+  return (
+    <Fragment>
+      <Label onClick={() => handleChange()}>{label}</Label>
+      <Container checked={checked} onClick={() => handleChange()}>
+        <input type="checkbox" defaultChecked={checked} />
+      </Container>
+    </Fragment>
+  );
+};
+
+export default CheckBox;
+
+const Container = styled(({ checked, children, ...props }) => (
+  <div {...props}>{children}</div>
+))<{
+  checked: boolean;
+}>`
+  position: relative;
+  width: 30px;
+  height: 15px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+  :before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 15px;
+    height: 15px;
+    border-radius: 100%;
+    background: ${({ theme }) => darken(0.5, theme.primary)};
+    transition: 0.3s;
+    ${({ checked }) =>
+      checked &&
+      css`
+        background: ${({ theme }) => theme.secondary};
+        transform: translateX(15px);
+      `}
+  }
+  & > input {
+    display: none;
+  }
+`;
+
+const Label = styled.label`
+  font-size: ${rem(14)};
+  color: ${({ theme }) => theme.supportText};
+  cursor: pointer;
+`;
---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,63 @@
+import React, { useState, Fragment } from 'react';
+import styled from 'styled-components';
+import { rem } from 'polished';
+import { SelectInterface } from '@src//core/interfaces/SelectInterface';
+
+const Select: React.FC<SelectInterface> = ({
+  label,
+  initValue,
+  onChange,
+  children,
+}) => {
+  return (
+    <Fragment>
+      <Label>{label}</Label>
+      <SelectContainer>
+        <Arrow />
+        <StyledSelect
+          value={initValue}
+          onChange={(e) => onChange && onChange(e.target.value)}>
+          {children}
+        </StyledSelect>
+      </SelectContainer>
+    </Fragment>
+  );
+};
+
+export default Select;
+
+const SelectContainer = styled.div`
+  position: relative;
+`;
+
+const StyledSelect = styled.select`
+  padding: 2px 15px 2px 10px;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  -ms-progress-appearance: none;
+  appearance: none;
+  background-color: transparent;
+  border-radius: 3px;
+  font-size: ${rem(12)};
+  cursor: pointer;
+`;
+
+const Arrow = styled.div`
+  position: absolute;
+  top: -3px;
+  right: 5px;
+  bottom: 0;
+  margin: auto;
+  height: 5px;
+  width: 5px;
+  border: 2px solid ${({ theme }) => theme.secondary};
+  border-left: 0;
+  border-top: 0;
+  transform: rotate(45deg);
+`;
+
+const Label = styled.label`
+  font-size: ${rem(14)};
+  color: ${({ theme }) => theme.supportText};
+  cursor: pointer;
+`;
